b2f4a02b24675cd23cc96c59c467889ae9816097
Aidan Border <planes4u@outlook.com>
2023-09-05 12:20:33 -0400

    Structure changes and adding login.js

--- cs-senior-project/src/app/page.js
+++ cs-senior-project/src/app/page.js
@@ -1,4 +1,6 @@
-import react from "react";
+'use client'
+
+import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,39 +10,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './page.module.css';
 
 export default function Home() {
+
   return (
     <div>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-        crossorigin="anonymous"
-      />		
       <title>Turing Test-Inator!</title>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ChatGPTuring</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#game">Game</Nav.Link>
+            <Nav.Link href="#write">Write</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#login">Login</Nav.Link>
+            <Nav.Link href="#signup">Sign Up</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
       <br />
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <div id="container">
+      <div className="container">
         <h1>Home Page</h1>
         <p />
         <p>
