0bb46fbc2521c95964495793b67c166423af989b
Aidan Border <planes4u@outlook.com>
2023-09-12 12:22:27 -0400

    Refactoring init-firebase

--- src/app/init-firebase.js
+++ src/app/init-firebase.js
@@ -1,19 +1,22 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp, getApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
-// import { } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js"
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp } from "firebase/compat/app";
+import "firebase/compat/auth"
 // import { } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js"
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-// 	apiKey: "AIzaSyAThWUht-tNf-REso6KxH1tKQ3PXfeCifw",
-// 	authDomain: "cs-senior-project-9c0e0.firebaseapp.com",
-// 	projectId: "cs-senior-project-9c0e0",
-// 	storageBucket: "cs-senior-project-9c0e0.appspot.com",
-// 	messagingSenderId: "1054365365385",
-// 	appId: "1:1054365365385:web:fdd0bf6ce2f08ee09dc258"
-// };
+export default function InitFirebase() {
+	// Your web app's Firebase configuration
+	const firebaseConfig = {
+		apiKey: "AIzaSyAThWUht-tNf-REso6KxH1tKQ3PXfeCifw",
+		authDomain: "cs-senior-project-9c0e0.firebaseapp.com",
+		projectId: "cs-senior-project-9c0e0",
+		storageBucket: "cs-senior-project-9c0e0.appspot.com",
+		messagingSenderId: "1054365365385",
+		appId: "1:1054365365385:web:fdd0bf6ce2f08ee09dc258"
+	};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+	// Initialize Firebase
+	const firebaseApp = initializeApp(firebaseConfig);
+	return firebaseApp;
+}
