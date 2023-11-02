4ecc51c3e3d9793ae89def546e74d085ff9c3843
Aidan Border <planes4u@outlook.com>
2023-11-02 13:36:03 -0400

    Updating navbar for login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -23,8 +23,8 @@ export default function Login() {
 				<Container>
 					<Navbar.Brand href="/">ChatGPTuring</Navbar.Brand>
 					<Nav className="me-auto">
-						<Nav.Link href="./game">Game One</Nav.Link>
-						<Nav.Link href="./gameTwo">Game Two</Nav.Link>
+						<Nav.Link href="./game">Single Prompt</Nav.Link>
+						<Nav.Link href="./gameTwo">Dual Prompt</Nav.Link>
 					</Nav>
 				</Container>
 			</Navbar>
