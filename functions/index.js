edbd6e675d20748c387871471294359454ab9d62
Aidan Border <planes4u@outlook.com>
2023-11-25 16:29:57 -0500

    Refactoring to use the Firebase Admin SDK

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const {initalizeApp} = require("firebase-admin/app");
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require('firebase-functions/v1');
+import {
+	getFirestore, doc, setDoc
+} from 'firebase/firestore'
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -19,8 +24,21 @@ const {initalizeApp} = require("firebase-admin/app");
 //   response.send("Hello from Firebase!");
 // });
 
-initalizeApp();
-
 exports.createNewUserDoc = functions.auth.user().onCreate((user) => {
-  // user.
+  firestore.
+
+  admin.firestore.CollectionReference()(doc(db, "users", authResult.user.uid), {
+    hasGuessed: false,
+    isStudent: false,
+    guessed: 0,
+    correctness: 0,
+    userID: authResult.user.uid,
+    gradeLevel: "test",
+    correctAIGameOne: 0,
+    correctHumanGameOne: 0,
+    correctnessGameTwo: 0,
+    guessedAIGameOne: 0,
+    guessedGameTwo: 0,
+    guessedHumanGameOne: 0
+  }, {merge: true})
 });
